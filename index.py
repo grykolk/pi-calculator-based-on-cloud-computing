44a32108384953ed02a66147695d109453ba702d
horo <grykolk@outlook.com>
2019-05-16 05:03:39 -0700

    finish the EMR part

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-
+import time
 import os
 import webapp2
 import jinja2
@@ -11,13 +11,15 @@ import numpy as np
 import boto
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
-import boto.emr
+#import boto.emr
 from boto.emr.connection import EmrConnection
 from boto.emr.step import StreamingStep
 from boto.emr.instance_group import InstanceGroup
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_environment = jinja2.Environment( loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+access_id=''
+access_key=''
 	
 def doRender(handler, tname, values={}):
 	temp = os.path.join(os.path.dirname(__file__), 'templates/'+tname)
@@ -32,12 +34,14 @@ def doRender(handler, tname, values={}):
 	template = jinja_environment.get_template(tname)
 	handler.response.out.write(template.render(newval))
 	return True
+
 def lambda_call(shots,Q,queue):
     c = httplib.HTTPSConnection("b5e5hz094g.execute-api.eu-west-2.amazonaws.com")
     json_send= '{ "shots_each_threat": "'+str(shots)+'","Q": "'+str(Q)+'"}'
     c.request("POST", "/default", json_send)
     response = c.getresponse()
     queue.put(json.loads(response.read()))
+
 def multithreading_lambda_call(shots,Q,thread_count):
     queue=Queue.Queue()
     threads=[]
@@ -51,9 +55,67 @@ def multithreading_lambda_call(shots,Q,thread_count):
     for i in range(thread_count):
         results[i*Q:(i*Q)+Q]=queue.get()
     return results
+
+def save_result(data,para):
+    s3_connection=S3Connection(access_id,access_key)
+    bucket=s3_connection.get_bucket('bucket774')
+    k=Key(bucket)
+    k.key='record.json'
+    k.set_contents_from_string(data)
+    k.key='record_para.json'
+    k.set_contents_from_string(para)
+
+def set_input_and_delete_output(R,Q,shotsForEachBlock):
+    emr_input=''
+    for i in range(R*Q):
+        emr_input+=str(shotsForEachBlock)
+        emr_input+='\n'
+    s3_connection=S3Connection(access_id,access_key)
+    bucket=s3_connection.get_bucket('bucket774')
+    for key in bucket.list(prefix='output/'):
+        key.delete()
+    k=Key(bucket)
+    k.key='input/0001'
+    k.set_contents_from_string(emr_input)
+
+def create_emr(R):
+    if not boto.config.has_section('Boto'):
+        boto.config.add_section('Boto')
+    boto.config.set('Boto','https_validate_certificates','False')
+    step=StreamingStep(name='MC_Method example', cache_files=['s3n://bucket774/map.py#map.py'],mapper='map.py',input='s3://bucket774/input/',output='s3://bucket774/output/')
+    conn=EmrConnection(access_id,access_key)
+    instance_groups=[]
+    instance_groups.append(InstanceGroup(num_instances=1,role="MASTER",type='m4.large',market="ON_DEMAND",name="Master nodes"))
+    if R>1:
+        instance_groups.append(InstanceGroup(num_instances=R-1,role="CORE",type='m4.large',market="ON_DEMAND",name="Slave nodes"))
+    cluster_id=conn.run_jobflow(name='test MC_method run',instance_groups=instance_groups,enable_debugging=False,steps=[step], visible_to_all_users=True, keep_alive=True, job_flow_role="EMR_EC2_DefaultRole",service_role="EMR_DefaultRole",hadoop_version='2.4.0',log_uri='s3://bucket774/log')
+    return cluster_id,conn
+def add_step_emr(conn,cluster_id):
+    step=StreamingStep(name='MC_Method example', cache_files=['s3n://bucket774/map.py#map.py'],mapper='map.py',input='s3://bucket774/input/',output='s3://bucket774/output/')
+    conn.add_jobflow_steps(cluster_id,step)
+def get_output():
+    data=''
+    conn=S3Connection(access_id,access_key)
+    bucket=conn.get_bucket('bucket774')
+    k=Key(bucket)
+    #k.key='output/part-00000'
+    for key in bucket.list(prefix='output/part'):
+        data+=key.get_contents_as_string()
+    data=data.replace('\t\n','')#remove format
+    data=data[:-1]#remove the final ","into the string
+    return json.loads('['+data+']')
+
+def in_circle_to_pi(PYdata,shotsForEachBlock):
+    for i in range(1,len(PYdata)):
+        PYdata[i]+=PYdata[i-1]
+	PYdata[i-1]/=shotsForEachBlock*(i)
+    PYdata[len(PYdata)-1]/=shotsForEachBlock*len(PYdata)
+    return json.dumps(PYdata)
+    
+
 class S3Handler(webapp2.RequestHandler):
     def post(self):
-        s3_connection=S3Connection('AKIAI7AQSIAKUCHT2IWA','sVOEy7qRNZ18Ef138lIRCsQCyzcRRlxE/1OD0GYl')
+        s3_connection=S3Connection(access_id,access_key)
         bucket=s3_connection.get_bucket('bucket774')
         k=Key(bucket)
         k.key='record.json'
@@ -78,68 +140,82 @@ class CalculateHandler(webapp2.RequestHandler):
                 shotsForEachThread=shots//R
                 shotsForEachBlock=shotsForEachThread//Q
 
-		if mode == 1 :
+		if mode == 1 :#mode=given accuracy
 			accuracy = int(self.request.get('accuracy'))
                         runtimes=0
                         found=0
                         PYdata=np.zeros(Q*R)#set as a numpy array
-                        while(found==0):
-                            runtimes+=1
-                            #use+=  here to cumulative value for each calculation
-                            PYdata +=multithreading_lambda_call(shotsForEachThread,Q,R)
-                            if(round(np.sum(PYdata)/(shots*runtimes),accuracy)==round(math.pi,accuracy)):
-                                found=1
+                        if server==0:#server=lanbda, mode =given accuracy
+                            while(found==0):
+                                runtimes+=1
+                                #use+=  here to cumulative value for each calculation
+                                PYdata +=multithreading_lambda_call(shotsForEachThread,Q,R)
+                                if(round(np.sum(PYdata)/(shots*runtimes),accuracy)==round(math.pi,accuracy)):
+                                    found=1
+                            #for i in range(1,len(PYdata)):
+			#	PYdata[i]+=PYdata[i-1]
+			#	PYdata[i-1]/=shotsForEachBlock*(i)*runtimes
+			 #   PYdata[len(PYdata)-1]/=shotsForEachBlock*len(PYdata)*runtimes
+			  #  data=json.dumps(PYdata.tolist())#covernt numpy array to list
+                        elif server==1:#server=emr, mode =given accuracy
+
+                            set_input_and_delete_output(Q,R,shotsForEachBlock)
+                            cluster_id,conn=create_emr(R)
+                            #deRender(self,'index.htm',{'note':'emr cluster created successful, please waiting for the calculator'})
+                            while (found==0):
+                                runtimes+=1
+                                complete=0
+                                while(complete==0):
+                                    status=conn.describe_cluster(cluster_id)
+                                    if(status.status.state=='WAITING'):
+                                        complete=1
+                                        PYdata+=get_output()
+                                    if(status.status.state=='TERMINATED_WITH_ERRORS'):
+                                        complete=1
+                                        error=1
+                                    else:
+                                        time.sleep(30)
+                                if(round(np.sum(PYdata)/(shots*runtimes),accuracy)==round(math.pi,accuracy)):
+                                    found=1
+                                    conn.terminate_jobflow(cluster_id)
+
+                                else:
+                                    add_step_emr(conn,cluster_id)
                         for i in range(1,len(PYdata)):
-				PYdata[i]+=PYdata[i-1]
-				PYdata[i-1]/=shotsForEachBlock*(i)*runtimes
+			    PYdata[i]+=PYdata[i-1]
+			    PYdata[i-1]/=shotsForEachBlock*(i)*runtimes
 			PYdata[len(PYdata)-1]/=shotsForEachBlock*len(PYdata)*runtimes
 			data=json.dumps(PYdata.tolist())#covernt numpy array to list
+
                         
 		
-		else:
-                    if server==0:
+                elif mode==0:#mode=given number
+                    if server==0:#server=lambda, mode=given number
 
 			PYdata =multithreading_lambda_call(shotsForEachThread,Q,R)
-			for i in range(1,len(PYdata)):
-				PYdata[i]+=PYdata[i-1]
-				PYdata[i-1]/=shotsForEachBlock*(i)
-			PYdata[len(PYdata)-1]/=shotsForEachBlock*len(PYdata)
-			data=json.dumps(PYdata)
-                    else:
-                        emr_input=''
-                        for i in range(R*Q):
-                            emr_input+=str(shotsForEachBlock)
-                            emr_input+='\n'
+			
+                    elif server==1:#server=emr,mode=give number
+                        set_input_and_delete_output(R,Q,shotsForEachBlock)
+                        cluster_id,conn=create_emr(R)
+                        #doRender(self,'index.htm',{'note':'emr cluster created successful, please waiting for the calculator'})
+                        complete=0
+                        error=0
+                        while(complete==0):
+                            status=conn.describe_cluster(cluster_id)
+                            if( status.status.state=='WAITING' ):
+                                complete=1
+                                PYdata=get_output()
+                            if (status.status.state=='TERMINATED_WITH_ERRORS'):
+                                complete=1
+                                error=1
+                            else:
+                                time.sleep(30)
+                                #doRender(self,'index.htm',{'note':'still in process:'+status.status.state})
+                        conn.terminate_jobflow(cluster_id)
+                    data=in_circle_to_pi(PYdata,shotsForEachBlock)
 
-                        s3_connection=S3Connection('AKIAI7AQSIAKUCHT2IWA','sVOEy7qRNZ18Ef138lIRCsQCyzcRRlxE/1OD0GYl')
-                        bucket=s3_connection.get_bucket('bucket774')
-                        for key in bucket.list(prefix='output/'):
-                            key.delete()
-                        k=Key(bucket)
-                        k.key='input/0001'
-                        k.set_contents_from_string(emr_input)
-                        step=StreamingStep(name='MC_Method example',mapper='s3://bucket774/map.py',reducer='s3://',input='s3://input/',output='s3://output/')
-                        #emr_conn=boto.emr.connect_to_region(region_name='eu-west-2',aws_access_key_id='AKIAI7AQSIAKUCHT2IWA',aws_secret_access_key='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',)
-                        emr_conn=EmrConnection('AKIAI7AQSIAKUCHT2IWA','sVOEy7qRNZ18Ef138lIRCsQCyzcRRlxE/1OD0GYl')
-                        instance_groups=[]
-                        instance_groups.append(InstanceGroup(num_instances=1,role="MASTER",type='m4.large',market="ON_DEMAND",name="Master nodes"))
-                        if R>1:
-                            instance_groups.append(InstanceGroup(num_instances=R-1,role="CORE",type='m4.large',market="ON_DEMAND",name="Slave nodes"))
-                        cluster_id=emr_conn.run_jobflow(name='test MC_method run',availability_zone='eu-west-2b',instance_groups=instance_groups,enable_debugging=False,steps=[step], visible_to_all_users=True, keep_alive=False, job_flow_role="EMR_EC2_DefaultRole",service_role="EMR_EC2_DefaultRole",hadoop_version='2.4.0')
-
-
-
-
-
-    		s3_connection=S3Connection('AKIAI7AQSIAKUCHT2IWA','sVOEy7qRNZ18Ef138lIRCsQCyzcRRlxE/1OD0GYl')
-                bucket=s3_connection.get_bucket('bucket774')
-                k=Key(bucket)
-                k.key='record.json'
-                k.set_contents_from_string(data)
-                k.key='record_para.json'
                 parameter='['+str(shotsForEachBlock)+','+str(R)+','+str(Q)+','+str(shots)+','+str(PYdata[len(PYdata)-1])+']'
-                #record_para=json.loads(parameter)
-                k.set_contents_from_string(parameter)
+                save_result(data,parameter)
                 doRender(self,'chart.htm',{'Data':data,'shots_each_threat':shotsForEachBlock,'R':R,'Q':Q,'pi':math.pi,'shots':shots,'result':PYdata[len(PYdata)-1]})
 
 
